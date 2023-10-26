7b2aec6448f28b934064fd52893f93672f3a17da
venkatesh1226 <venkatesh1226m@gmail.com>
2023-10-26 03:03:52 -0700

    UI changed

--- chatapp_react/src/components/chatlist/ChatList.jsx
+++ chatapp_react/src/components/chatlist/ChatList.jsx
@@ -1,47 +1,94 @@
-import React from "react";
+// import React from "react";
+// import Bubble from "../Bubble/Bubble";
+// import "./chatlist.css";
+// import { useState } from "react";
+
+// const ChatList = ({ user, chatWith, msgList, sendMsg }) => {
+//   const [txt, setTxt] = useState("");
+//   const handleChange = (event) => {
+//     setTxt(event.target.value);
+//   };
+//   return (
+//     <>
+//       <h1>Chating With {chatWith.userName} </h1>
+//       <div id="msg-list">
+//         {Array.isArray(msgList) &&
+//           msgList.map((msg) => {
+//             console.log(msg.sender);
+//             console.log(user);
+//             return (
+//               <Bubble
+//                 message={msg.message}
+//                 isMine={msg.sender.userId === user.userId}
+//                 key={msg.msgID}
+//               />
+//             );
+//           })}
+
+//         <div id="flex">
+//           <input
+//             type="text"
+//             placeholder="Enter your message here"
+//             onChange={handleChange}
+//             value={txt}
+//           />
+//           <button
+//             onClick={() => {
+//               setTxt("");
+//               sendMsg(txt);
+//             }}
+//           >
+//             Send
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ChatList;
+
+import React, { useState } from "react";
 import Bubble from "../Bubble/Bubble";
 import "./chatlist.css";
-import { useState } from "react";
 
 const ChatList = ({ user, chatWith, msgList, sendMsg }) => {
   const [txt, setTxt] = useState("");
+
   const handleChange = (event) => {
     setTxt(event.target.value);
   };
+
+  const handleSend = () => {
+    if (txt.trim() !== "") {
+      sendMsg(txt);
+      setTxt("");
+    }
+  };
+
   return (
-    <>
-      <h1>Chating With {chatWith.userName} </h1>
-      <div id="msg-list">
+    <div className="chat-list-container">
+      <h1>Chatting With {chatWith.userName}</h1>
+      <div className="message-container">
         {Array.isArray(msgList) &&
-          msgList.map((msg) => {
-            console.log(msg.sender);
-            console.log(user);
-            return (
-              <Bubble
-                message={msg.message}
-                isMine={msg.sender.userId === user.userId}
-                key={msg.msgID}
-              />
-            );
-          })}
-        <div id="flex">
-          <input
-            type="text"
-            placeholder="Enter your message here"
-            onChange={handleChange}
-            value={txt}
-          />
-          <button
-            onClick={() => {
-              setTxt("");
-              sendMsg(txt);
-            }}
-          >
-            Send
-          </button>
-        </div>
+          msgList.map((msg) => (
+            <Bubble
+              message={msg.message}
+              isMine={msg.sender.userId === user.userId}
+              key={msg.msgID}
+            />
+          ))}
       </div>
-    </>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message here"
+          onChange={handleChange}
+          value={txt}
+        />
+        <button onClick={handleSend}>Send</button>
+      </div>
+    </div>
   );
 };
 
